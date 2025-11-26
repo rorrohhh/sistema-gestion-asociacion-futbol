5f78ee2fee5c86c0590337738abb1bd8e77fa261
rorrohhh <roepze@gmail.com>
2025-11-26 16:29:30 -0300

    adicion de campos pasaporte y tipo verificacion en backend

---
+++ backend/config/config.js
@@ -0,0 +1,26 @@
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql"
+  },
+  test: {
+    // Usar la misma configuración para simplificar si no tienes una BD de test dedicada
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME + "_test", // Un nombre diferente es recomendado para test
+    host: process.env.DB_HOST,
+    dialect: "mysql"
+  },
+  production: {
+    // La configuración de producción siempre debe ser gestionada por variables de entorno
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME + "_production", // Un nombre diferente es recomendado
+    host: process.env.DB_HOST,
+    dialect: "mysql"
+  }
+};
